2ab871076ed6b86176fbbf769516d57e8da0e941
Prashant Panwar <prashant.panwar777@gmail.com>
2019-05-01 00:11:09 +0530

    feat: binding adapters

---
+++ app/src/main/java/com/example/movieapp/binding/FragmentBindingAdapters.kt
@@ -0,0 +1,18 @@
+package com.example.movieapp.binding
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.fragment.app.Fragment
+import javax.inject.Inject
+
+
+/**
+ * Binding adapters that work with a fragment instance.
+ */
+class FragmentBindingAdapters @Inject constructor(val fragment: Fragment)  {
+
+    @BindingAdapter("viewVisible")
+    fun showHide(view: View, show: Boolean) {
+        view.visibility = if (show) View.VISIBLE else View.GONE
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/binding/FragmentDataBindingComponent.kt
@@ -0,0 +1,15 @@
+package com.example.movieapp.binding
+
+import androidx.databinding.DataBindingComponent
+import androidx.fragment.app.Fragment
+
+/**
+ * A Data Binding Component implementation for fragments.
+ */
+class FragmentDataBindingComponent(fragment: Fragment) : DataBindingComponent {
+
+    private val adapter = FragmentBindingAdapters(fragment)
+
+    override fun getFragmentBindingAdapters() = adapter
+
+}
